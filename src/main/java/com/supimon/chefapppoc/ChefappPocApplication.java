ac05b8fe52ba5af3e900cfbd999534b16b70280b
Supi <supimon@gmail.com>
2020-01-02 09:18:24 +0530

    first commit

---
+++ src/main/java/com/supimon/chefapppoc/ChefappPocApplication.java
@@ -0,0 +1,13 @@
+package com.supimon.chefapppoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefappPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChefappPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supimon/chefapppoc/ChefappPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supimon.chefapppoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChefappPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
